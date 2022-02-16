b537d66ef9086ba45512d27645debf732d1bf073
marco bonola <marco.bonola@gmail.com>
2022-02-16 16:15:57 +0100

    first v2 commit. to be tested

--- bplustree.c
+++ bplustree.c
@@ -21,114 +21,170 @@
 	})
 
 
-#define INDEX_MAP_SIZE	 	1024
-#define DATA_MAP_SIZE	 	1024
-#define TREE_HEIGTH		3	
-#define NODE_SIZE		5
+#define NODE_ORDER 4 			//must be hardcoded
+#define MAX_TREE_HEIGHT 8 		//must be hardcoded
+#define INDEX_MAP_SIZE 65536		//must be hardcoded
 
-struct index_map_val {
-	 __u32 key;
-	 __u32 pointer;
+struct bplus_tree_info {
+	int curr_root;			//initialized to 0
+	int curr_h;			//initialized to 0
+	int free_indexes_tail;		//initialized to INDEX_MAP_SIZE -1
+	int is_full;			//initialized to 0
 };
 
+struct bplus_node_entry {
+	__u64 pointer;
+	__u64 key;
+};
+
+struct bplus_node {
+	struct bplus_node_entry entry[NODE_ORDER];
+};
+
+//this map contains the b+ tree index pages
 struct bpf_map_def SEC("maps") index_map = {
 	.type = BPF_MAP_TYPE_ARRAY,
 	.key_size = sizeof(__u32),
-	.value_size = sizeof(struct index_map_val),
+	.value_size = sizeof(struct bplus_node),
 	.max_entries = INDEX_MAP_SIZE,
-	.map_flags = 0,
 };
 
-struct bpf_map_def SEC("maps") data_map = {
+//this map contains the b+ tree info (1 entry)
+struct bpf_map_def SEC("maps") tree_info = {
+	.type = BPF_MAP_TYPE_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(struct bplus_tree_info),
+	.max_entries = 1,
+};
+
+//this map contains the b+ tree free node indexes
+struct bpf_map_def SEC("maps") free_index_list = {
 	.type = BPF_MAP_TYPE_ARRAY,
 	.key_size = sizeof(__u32),
 	.value_size = sizeof(__u32),
-	.max_entries =DATA_MAP_SIZE,
-	.map_flags = 0,
+	.max_entries = INDEX_MAP_SIZE,
 };
+
+#define OP_SEARCH 1
+#define OP_INSERT 2
+#define OP_DELETE 3
 
 struct payload {
 	__u32 key;
 };
 
-static inline void * __bplus_search(__u32 search_key) {
-	int i, j;
-	__u32 index = 0;
-	__u32 *data = NULL;
-	struct index_map_val * val0 = NULL;
-	struct index_map_val * val1 = NULL;
-	int found = 0;
+static inline __u64 __bplus_process(int cmd, __u64 key, __u8 *data, int len) {
+	__u32 zero = 0;
+	struct bplus_tree_info *info = NULL;
+	struct bplus_tree_info updated_info = {};
+	struct bplus_node *node = NULL;
+	int node_index = 0;
+	__u64 data_pointer = 0;
+	int is_leaf = 0;
+	int node_traversed[MAX_TREE_HEIGHT] = {0};
+	int i=0, j=0;
 
+	info = bpf_map_lookup_elem(&tree_info, &zero);
+	if (!info) {
+		return XDP_ABORTED;
+	}
+	
+	__builtin_memcpy(&updated_info, info, sizeof(struct bplus_tree_info));
 
-	for (i=0; i<TREE_HEIGTH; i++) {
-    		val0 = bpf_map_lookup_elem(&index_map, &index);
-		if (val0 == NULL) {
-			return NULL;
+	//all operations need to first search for the leaf node in which 
+	//we search/insert/delet a key. we always start from the root	
+	node_index = info->curr_root;
+	
+	for (i=0; i<MAX_TREE_HEIGHT; i++) {
+		if (i==info->curr_h-1){
+			is_leaf = 1;
 		}
-		for (j=0; j<NODE_SIZE-1; j++) {
-			//for now, just for simplicity, we use linear search
-			//TODO do a binary search
-			if (i != TREE_HEIGTH -1) { //we are in the root or in an intermediate node
-				bpf_printk("NON LEAF i %d j %d\n",i, j );
-				bpf_printk("comparing %d with %d\n", search_key, val0->key);
-				if (search_key < val0->key) {
-					bpf_printk("LESS\n");
-					index = val0->pointer; 	
-					bpf_printk("index = %d\n", val0->pointer);
+    		node = bpf_map_lookup_elem(&index_map, &node_index);
+
+		if (!node) {
+			return XDP_ABORTED;
+		}
+
+		if (node->entry[NODE_ORDER].key == 0) {
+			//node is empty
+			break;
+		}
+		node_traversed[i] = node_index;
+
+		for (j=0; j<NODE_ORDER-1; j++) {
+			if (is_leaf == 0) {
+				//for now we implement a linear search in the node
+				if (key < node->entry[j].key) {
+					node_index = node->entry[j].pointer;
 					break;
 				} else {
-					index ++;
-					val1 = bpf_map_lookup_elem(&index_map, &index);
-					if (val1 == NULL) {
-						return NULL;
-					}
-					if ((search_key == val0->key) || (val1->key == 0) ) {
-						bpf_printk("EQUAL or NEXT 0\n");
-						index = val1->pointer;
-						bpf_printk("index = %d\n", val1->pointer);
-						break;
-					} else { //i need the next element
-						bpf_printk("GREATER\n");
-						if (j == NODE_SIZE - 2) { //last element
-							index = val1->pointer;
-							bpf_printk("LAST element index = %d\n", val1->pointer);
-							break;
-						} else {
-							val0 = val1; 
-						}
+					//(search key == curr key) or
+					if ((key == node->entry[j].key) || 
+					//(search key > curr_key) and (curr key == last key)
+					  ( (j == node->entry[NODE_ORDER-1].key) || (j == NODE_ORDER-2)) ) { 
+						node_index = node->entry[j+1].pointer;
+						break;	
 					}
 				}
-			} else { //we are in a leaf node
-				bpf_printk("LEAF i %d j %d\n",i, j );
-				bpf_printk("search key %d cur key %d\n", search_key, val0->key);
-				if (search_key == val0->key) {
-					index = val0->pointer;		
-					found = 1; //we can spare this variable if we don't use data index 0
-					bpf_printk("LEAF NODE found index %d\n", index);
-					break;
-				} else {
-					index ++;
-					val0 = bpf_map_lookup_elem(&index_map, &index);
-					if (val0 == NULL) {
-						return NULL;
-					}
+			}
+			else {
+				//leaf node
+				if (cmd == OP_SEARCH) {
+					//exact match of the search key in the leaf node
+					for (j=0; j<NODE_ORDER-2; j++) {
+						if (key == node->entry[j].key) {
+							data_pointer = node->entry[j].pointer;
+							break;
+						} 				
+					}	
 				}
 			}
 		}
 
+		if (is_leaf) {
+		       break;
+		}	       
 	}
-	
-	if (!found) {
-		bpf_printk("not found\n");
-	} else {
-		data = bpf_map_lookup_elem(&data_map, &index);
-		if (data) {
-			return data;
+
+	if (cmd == OP_SEARCH) {
+		return data_pointer; 
+	} else if (cmd == OP_INSERT) {
+		//node == leaf node
+		int num_of_keys_in_node = node->entry[NODE_ORDER -1].key;
+		__u64 free_data_index = 1234;
+		int memsize = 0;
+		if (num_of_keys_in_node == NODE_ORDER - 1) { //the leaf node is full
+			//TODO split!!
+		} else {
+			//ordered insertion in array
+			for (i==0; i<NODE_ORDER-2; i++) {
+
+				if (i == num_of_keys_in_node-1) {//empty entry
+					node->entry[i].key = key;	
+					break;
+				}
+
+				if (key < node->entry[i].key) {
+					//move and set
+					//TODO get free data map idx
+					//TODO insert in data map
+					memsize = sizeof(struct bplus_node_entry)*(num_of_keys_in_node-i);
+					
+					__builtin_memcpy(&(node->entry[i+1]), &(node->entry[i]), memsize);
+					node->entry[i].pointer = free_data_index;
+					node->entry[i].key = key;
+				}
+			}
 		}
-	} 
-	
-	return NULL;
+		return free_data_index;	
+	} else if (cmd == OP_DELETE) {
+		//index == leaf node index
+		return XDP_ABORTED;
+	} else {
+		return XDP_ABORTED;
+	}
 }
+
 
 SEC("xdp")
 int ingress(struct xdp_md *ctx) {
@@ -139,7 +195,7 @@ int ingress(struct xdp_md *ctx) {
 	struct udphdr *udp = (void*)ip + sizeof(struct iphdr);
 	struct payload *p = (void*)udp + sizeof(struct udphdr);
 	__u32 offset = 0;
-	__u32 *val = NULL;
+	__u64 ret = 0;
 	__be32 saddr = 0, daddr = 0;
         __be16 sport = 0, dport = 0;
 	__u8 h_source[6] = {0}, h_dest[6] = {0}; 
@@ -191,7 +247,8 @@ int ingress(struct xdp_md *ctx) {
 				return XDP_DROP;
 			}	
 
-			val = __bplus_search(p->key);
+			//val = __bplus_search(p->key);
+			ret = __bplus_process(OP_SEARCH, p->key, NULL, 0);
 		}
 
 		__builtin_memcpy(eth->h_dest, h_source, 6);
@@ -202,12 +259,13 @@ int ingress(struct xdp_md *ctx) {
 		udp->dest = sport;
 
 		
-
-		if (!val) { 
+		
+		if (!ret) { 
 			__u32 tmp = 0;
 			__builtin_memcpy(p, &tmp, 4);
 		} else {
-			__builtin_memcpy(p, val, 4);
+			//__builtin_memcpy(p, val, 4);
+			return XDP_ABORTED;
 		}
 
 		return XDP_TX;
